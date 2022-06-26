1553a8783138a9c5f6f2693bf61abf291f2552e7
Jose Tizon <josetizon7@gmail.com>
2022-06-25 19:47:34 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Quotes from './components/Quotes'
+
+function App() {
+
+  return (
+    <div className='Container'>
+      <div  className="App">
+        <Quotes />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Quotes.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import quotes from "../quotes.json";
+
+const Quotes = () => {
+
+    const random = Math.floor(Math.random() * quotes.length)
+
+    const [quoteNum, setQuoteNum] = useState(random)
+    const nextQuote = () => setQuoteNum(random)
+
+    const colors = ["#845EC2", "#00C9A7", "#D65DB1", "#4D8076", "#FF9671", "#C34A36", "#3596B5"]
+    const randomColorIndex = Math.floor(Math.random() * colors.length)
+    const color = colors[randomColorIndex]
+
+    document.body.style = `background: ${color}`
+
+    return (
+        <div style={{color: color}}>
+            <p><i className="fa-solid fa-quote-left"></i> {quotes[quoteNum].quote}</p>
+            <p>{quotes[quoteNum].author}</p>
+            <button onClick={nextQuote} style={{backgroundColor: color}}><i className="fa-solid fa-shuffle"></i></button>
+        </div>
+    );
+};
+
+export default Quotes;
